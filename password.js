ba86a6ca94caad4f5e9c63562f025622986d6938
Lorna Lynch <lornalynchit@yahoo.com>
2019-11-18 22:16:51 -0500

    added more values and variables wont work

--- password.js
+++ password.js
@@ -7,5 +7,13 @@ function generate(){
     // possible password values
     let values = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrxtuvwxyz1234567890!@#$%^&*()_+";
 
-    let password = ""    
+    let password = "";  
+    
+    // create for loop to choose password characters
+    for(var i = 0; i <= complexity; i++){
+        password = password + values.charAt(Math.floor(Math.random() * Math.floor(values.length - 1)));
+    }
+
+    // add password to textbox/
+    document.getElementById("display").value = password;
 }
