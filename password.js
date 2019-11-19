acd27ff02c1d9a0d94ae2aa24f0720b8cd3b58c6
Lorna Lynch <lornalynchit@yahoo.com>
2019-11-18 21:16:29 -0500

    added values and variables to js

---
+++ password.js
@@ -0,0 +1,11 @@
+// generate random password 
+function generate(){
+   
+    // set password length/complexity
+    let complexity = document.getElementById("slider").value;
+
+    // possible password values
+    let values = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrxtuvwxyz1234567890!@#$%^&*()_+";
+
+    let password = ""    
+}
