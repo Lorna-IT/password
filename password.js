0fd9786d2577b5273846f7be3a8c41d48946c265
Lorna Lynch <lornalynchit@yahoo.com>
2019-11-26 18:34:54 -0500

    got help with Eli renamed functions

--- password.js
+++ password.js
@@ -1,28 +1,32 @@
 // generate random password 
 function lowerCase() {
     var lcCharset = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
-    return lcCharset[Math.floor(Math.random() * lcCharset.length)]
+    return lcCharset
+    //[Math.floor(Math.random() * lcCharset.length)]
 }
     
 function upperCase() {
     var ucCharset = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
-    return ucCharset[Math.floor(Math.random() * ucCharset.length)]
+    return ucCharset
+    //[Math.floor(Math.random() * ucCharset.length)]
 }
 
 function numbers () {
     var nmCharset = ["1","2","3","4","5","6","7","8","9","0"]
-    return nmCharset[Math.floor(Math.random() * nmCharset.length)]
+    return nmCharset
+    //[Math.floor(Math.random() * nmCharset.length)]
 }
 
 function special () {
     var spCharset = ["!","@","#","$","%","^","&","*","(",")","?","=","+"]
-    return spCharset[Math.floor(Math.random() * spCharset.length)]
+    return spCharset
+    //[Math.floor(Math.random() * spCharset.length)]
 }
 
-var finalPass1 = []
+var finalPass1 = ""
 
-var finalPass2 = ""
-var finalPass3 = ""
+// var finalPass2 = ""
+// var finalPass3 = ""
 
 var pLength = 8
 
@@ -54,42 +58,45 @@ function uInput () {
     } else {
         spInclude = false
     }
-
+    createPass()
 }
 function sanitize() {
     if (pLength <8 || pLength > 28) {
         alert("please select more options")
     } else {
-
+        return
     }
-    finalPass1.length = 0;
+    //finalPass1.length = 0;
 }
 function createPass () {
     var fns = []
     if (lcInclude === true) {
-        fns.push(lowerCase)
+        fns.push(lowerCase())
     }
     if (ucInclude === true) {
-        fns.push(upperCase)
+        fns.push(upperCase())
     }
     if (nmInclude === true) {
-        fns.push(numbers)   
+        fns.push(numbers())   
     } 
     if (spInclude === true) {
-        fns.push(special)
+        fns.push(special())
     }
+
+    fns = fns.flat()
 
     // for loop
     for (i = 0; i < pLength; i++) {
         var rando = Math.floor(Math.random() * fns.length)
-        finalPass1.push(fns[rando]())
+        finalPass1 += fns[rando]
     }
 }
-function convert () {
-    finalPass2 = (finalPass1.toString())
-    finalPass3 = (finalPass2.replace(""))
-}
-function releasePass() {
-    (document.getElementById("passOut").innerHTML = finalPass3)
-    (document.getElementById("passOut").style.display = "inline-block");
-}
+
+// function convert () {
+//     finalPass2 = (finalPass1.toString())
+//     finalPass3 = (finalPass2.replace(""))
+// }
+// function releasePass() {
+//     (document.getElementById("passOut").innerHTML = finalPass3)
+//     (document.getElementById("passOut").style.display = "inline-block");
+// }
